2a8e1b9f5ad3bd647f2331a64e81d68dcdc99d94
Aaron Ostrovsky <aarondostrovsky+github@gmail.com>
2018-12-11 16:52:25 +0000

    get the average of a stimulus set

--- xxx.R
+++ xxx.R
@@ -60,3 +60,31 @@ for (block in 0:4){
   }
 }
 
+# I want to be able to look through the list and get a sublist that can
+# be used to perform functions on them, such as averaging.
+# I should also decide if I want to store any raw data in this list, 
+# although that might be too heavy a solution for memory usage. Better
+# might be either lots of reading in and out from the file system nrrds, or 
+# to continue using the HDF5 connection for subsetting things.
+stim2<-c(2,6,10,14,18)
+
+average<-array(data=0,dim = c(350,256,181))
+# dim(average)<-c(350,256,181)
+for (animal in stim2){
+  start<-presentationList[[animal]]$start
+  end<-presentationList[[animal]]$end
+  rangeOfImages<-seq(from=start,to=end,by=presentationList[[animal]]$timeResampled)
+  print("downsampling...\n")
+  print(dim(average))
+  downSampledImage<-apply(
+    imageDataSlice[rangeOfImages,,,,],
+    1,
+    function(x) resizeImage(x,350,256))
+  dim(downSampledImage)<-c(350,256,length(rangeOfImages))
+  print("making the DFF...\n")
+  print(dim(downSampledImage))
+  average<-average+makeDFF(downSampledImage,xyzDimOrder = c(1,2,3),backgroundSlices=c(75:85))
+  
+}
+average<-average/4
+write.nrrd(average,file.path(nrrdFiles,"Average_stim2.nrrd"))
