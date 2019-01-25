b7837e2f0c6889071c6607eea50e776b219238c0
Aaron Ostrovsky <aarondostrovsky+github@gmail.com>
2019-01-25 14:54:13 +0000

    add a small amount of code to try to rationalize how to handle different stimulation protocols

--- StartUp_SLMCodeR.R
+++ StartUp_SLMCodeR.R
@@ -10,11 +10,16 @@ require('ggplot2')
 require('squash')
 require('digest')
 
-pixelOffset=399
-
 LSMCodeRConfig<-list()
 LSMCodeRConfig$srcdir<-normalizePath(dirname(attr(body(function() {}),'srcfile')$filename))
 LSMCodeRConfig$maindir<-dirname(LSMCodeRConfig$srcdir)
 LSMCodeRConfig$logDir <- file.path(LSMCodeRConfig$srcdir,"logs")
+LSMCodeRConfig$protocolDir <- file.path(LSMCodeRConfig$srcdir,"protocolCSVs")
 
 source(file.path(LSMCodeRConfig$srcdir,"calciumImagingFunctions.R"))
+
+pixelOffset=399
+sabineProtocolSimple <- list(
+  presentationMatrix = matrix(rep(c(3,4,5,6),5),nrow = 5,byrow=TRUE),
+  stimulationSections = read.csv(file.path(LSMCodeRConfig$protocolDir,"sabineProtocolSimple"))
+)
