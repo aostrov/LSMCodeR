6e52046860e5eca8364c0f6be0b416022c733ba7
Aaron Ostrovsky <aarondostrovsky+github@gmail.com>
2019-02-13 10:28:01 +0000

    isolating the code for finding green flashes and calling the log parsing files
    * at some point I could wrap this all up as a function, but for now I like that
    it has to interact with the global environment so much

---
+++ parseImageForGreenFlashAndLSMandStimLogs.R
@@ -0,0 +1,46 @@
+#################
+# file handling #
+#################
+
+# open the connection to the .mat file
+file.h5 <- H5File$new(myFile, mode = "r+")
+# save a new variable that contains the image data
+# this step can be avoided, and might improve performance on very large datasets
+imageDataSlice<-file.h5[["imagedata"]]
+file.h5$close()
+# file.h5$close()
+# data is in the form:
+# [stacks,(channels),slices,rows, columns]
+imageDataSlice.dims <- imageDataSlice$dims
+# frames channels slices rows  columns
+# 33001     1        1    512   700
+
+#################
+## Green Flash ##
+#################
+flashmean <- mean(imageDataSlice[1:100,,,10:20,10:20])
+flashsd <- sd(imageDataSlice[1:100,,,10:20,10:20])
+
+slicesWithBigNumbers.begin <- c()
+for (i in 1:100){
+  if (mean(imageDataSlice[i,,,,])>(flashmean+2*flashsd)){
+    slicesWithBigNumbers.begin <- c(slicesWithBigNumbers.begin,i)
+  }
+}
+
+startOfStimulations <- max(slicesWithBigNumbers.begin) # last slice of starting green flash
+# get the salient frames for the second green flash 
+
+slicesWithBigNumbers.end <- c()
+for (i in (33000-2000):33000){
+  if (mean(imageDataSlice[i,,,10:20,10:20])>(flashmean+2*flashsd)){
+    slicesWithBigNumbers.end <- c(slicesWithBigNumbers.end,i)
+  }
+}
+endOfStimulations <- min(slicesWithBigNumbers.end) # first slice of ending flash
+
+endOfFirstGreenFlash <- startOfStimulations + 1
+
+
+source(file.path(LSMCodeRConfig$srcdir,"stimLogParser.R"))
+source(file.path(LSMCodeRConfig$srcdir,"LSMLogParser.R"))
