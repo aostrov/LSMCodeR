b4c26cdd249b707c71f387c20ba3dd510165a203
Aaron Ostrovsky <aarondostrovsky+github@gmail.com>
2019-05-03 15:53:06 +0100

    analysis.R gets animalSummaryDF2

--- analysis.R
+++ analysis.R
@@ -67,8 +67,67 @@ ggplot(subset(analysisDF,
               background.mean>50 &
                 background.mean<400 & 
                 dff.max > 0.75 &
-                snr.mean > 3,animal!="AAMA"),
+                snr.mean > 3 & animal!="AAMA"),
        aes(geno,snr.max)) + 
   geom_boxplot(notch = T)
 
 ggplot(analysisDF) + geom_histogram(aes(geno),stat="count")
+
+# Set things up to get average data by animal
+animalSummaryDF2 <- data.frame(dff.mean=double(),
+                               snr.mean=double(),
+                               background.mean=double(),
+                               #animal=character(),
+                               geno=character(),
+                               animalKey=character(),
+                               bestStim=character())
+
+countingDF <- data.frame(animal=character(),
+                         geno=character())
+
+animals2=unique(substr(unique(analysisDF$animal),1,3))
+
+for (animal in animals2){
+  tempDF.animals <- analysisDF[grepl(paste("^",animal,sep=""),analysisDF$animal),]
+  tempDF.animals.subset <- subset(tempDF.animals,
+                                  background.mean>50 &
+                                    background.mean<400)
+  # stimuls.block
+  # get block 1
+  block1 <- tempDF.animals.subset[grepl("[1-4].1",tempDF.animals.subset$stimulus),]
+  block1.sub <- subset(block1,
+                       background.mean>10 &
+                         background.mean<400)
+  stims <- unique(block1.sub$stimulus)
+  response=0
+  bestResponse=character()
+  for (stim in stims){
+    x=block1.sub[block1.sub$stimulus==stim,"dff.max"]
+    if (mean(x)>response) {
+      response = mean(x)
+      bestResponse = stim
+    }
+  }
+  
+  tempDF.animals.subset.bestStim <- subset(tempDF.animals.subset, stimulus==bestResponse)
+  
+  if (nrow(tempDF.animals.subset.bestStim)>0) {
+    tempSummary=data.frame(dff.mean=mean(tempDF.animals.subset.bestStim$dff.max[is.finite(tempDF.animals.subset.bestStim$dff.max)]),
+                           snr.mean=mean(tempDF.animals.subset.bestStim$snr.max[is.finite(tempDF.animals.subset.bestStim$snr.max)]),
+                           background.mean=mean(tempDF.animals.subset.bestStim$background.mean[is.finite(tempDF.animals.subset.bestStim$background.mean)]),
+                           #animal=unique(tempDF.animals.subset.bestStim$animal),
+                           geno=unique(tempDF.animals.subset.bestStim$geno),
+                           animalKey=animal,
+                           bestStim=bestResponse)
+    
+    animalSummaryDF2 <- rbind(animalSummaryDF2,tempSummary)
+    countingDF <- rbind(countingDF,data.frame(animal=animal,geno=unique(animalSummaryDF2[animalSummaryDF2$animalKey==animal,"geno"])))
+  }
+  
+}
+
+
+ggplot(subset(animalSummaryDF2, animalSummaryDF2$animal!="AAM" ),
+       aes(geno,dff.mean)) + 
+  geom_boxplot(notch = F) +
+  xlab("Genotype") + ylab("DF/F") + theme(legend.position = "none") #+ geom_jitter(aes(color=animalKey))
